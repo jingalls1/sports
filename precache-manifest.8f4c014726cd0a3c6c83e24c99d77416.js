27b23283654ede9ab83f2c2a075aadbcbd34a263
Jacob Ingalls <jingalls@uoregon.edu>
2019-04-17 14:47:57 -0700

    Updates

---
+++ precache-manifest.8f4c014726cd0a3c6c83e24c99d77416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/sports/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cc27db115d99e5493758",
+    "url": "/github.io/sports/static/js/runtime~main.76494504.js"
+  },
+  {
+    "revision": "e25b604f12868d4aad63",
+    "url": "/github.io/sports/static/js/main.2c8bd047.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/github.io/sports/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e25b604f12868d4aad63",
+    "url": "/github.io/sports/static/css/main.1f9c0b6a.chunk.css"
+  },
+  {
+    "revision": "add7aac8eda560d130e925e901726276",
+    "url": "/github.io/sports/index.html"
+  }
+];
